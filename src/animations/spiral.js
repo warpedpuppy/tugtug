7d7d6907b846f7b1eb447477261f92534c73e999
ted <ted@warpedpuppy.com>
2018-04-27 19:51:14 +0100

    added resize to spiral

--- src/animations/spiral.js
+++ src/animations/spiral.js
@@ -70,7 +70,9 @@ export default function Spiral (THREE) {
 		   
 		},
 		resize: function () {
-			this.setup();
+			this.camera.aspect = window.innerWidth / window.innerHeight;
+			this.camera.updateProjectionMatrix();
+			this.renderer.setSize( window.innerWidth, window.innerHeight );
 		},
 		animate: function () {
 
