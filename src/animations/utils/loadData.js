de965f9df456f36f721d49529a051f1bfd380b29
ted <ted@warpedpuppy.com>
2019-11-10 11:55:17 -0500

    switched to heroku

--- src/animations/utils/loadData.js
+++ src/animations/utils/loadData.js
@@ -7,15 +7,15 @@ export default function () {
            let indexToGet = (this.grid.boards)?this.grid.boards.length:0;
            let next = indexToGet + 1;
            let requestBoardNumber = (indexToGet === 0)?1:next;
-
+           // console.log(`${API_BASE_URL}/api/tugtug/grids`)
            axios
-           .post(`${API_BASE_URL}/grids`, {board: requestBoardNumber})
+           .post(`${API_BASE_URL}/api/tugtug/grids`, {board: requestBoardNumber})
            .then(response => {
                 this.dbData = response.data;
-                console.log('loaded data = ', this.dbData.boards)
+               // console.log('loaded data = ', this.dbData.boards)
                 if (indexToGet === 0) {
                     this.grid.boards = [...this.grid.boards, response.data.boards];
-                    console.log(this.grid.boards)
+                   // console.log(this.grid.boards)
                     this.buildGame();
                  } else {
                     if (response.data.boards) {
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://tugtugbackend.tahdr7ifjc.us-west-2.elasticbeanstalk.com';
+    process.env.REACT_APP_API_BASE_URL || 'https://young-fjord-11847.herokuapp.com';
 
 // export const API_BASE_URL =
-//     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+//     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
